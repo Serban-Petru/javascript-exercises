5823628878e6a045276248c012833404615c3d09
Serban-Petru <agustonynon@yahoo.com>
2024-08-04 23:30:31 +0300

    Am terminat ex 3, 4 si 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;    let index = 0;
+    
+    if((a < 0) || (b<0) || (Number.isInteger(a) == false) || (Number.isInteger(b) == false)){ 
+        return 'ERROR';
+    }
 
+    if(b<a){
+        index=b;
+        b=a;
+        a=index;
+    }
+    for(let i=a; i<=b; i++)
+        sum+=i;
+    return sum;
 };
 
 // Do not edit below this line
