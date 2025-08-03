fbd8f319a05536784c686f1e7ddbcabbfcc2352f
Serban-Petru <177335339+Serban-Petru@users.noreply.github.com>
2025-08-03 19:56:15 +0000

    ex calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((acum, curentV) =>acum + curentV, 0 );
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acum, curentV) =>acum * curentV, 1 );
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let contor = 1;
+	for(let i = 1; i <= a; i++)
+    contor *=i;
+  return contor;
 };
 
 // Do not edit below this line
